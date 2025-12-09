a2a4d70bc25730a6989d1618cb743f4adc0f9b31
David Molineus <david.molineus@netzmacht.de>
2025-12-09 14:43:11 +0100

    Update phpcq tools and fix detected issues

--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -1,24 +1,11 @@
 <?php
 
-/**
- * Contao Bootstrap
- *
- * @package    contao-bootstrap
- * @subpackage Tab
- * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2013-2020 netzmacht David Molineus. All rights reserved.
- * @license    LGPL-3.0-or-later https://github.com/contao-bootstrap/tab/blob/master/LICENSE
- * @filesource
- */
+declare(strict_types=1);
 
-/*
- * Labels
- */
+// Labels
 $GLOBALS['TL_LANG']['CTE']['bootstrap_tabs'] = 'Tab elements';
 
-/*
- * content elements
- */
+// content elements
 $GLOBALS['TL_LANG']['CTE']['bs_tab_start'][0]     = 'Tab Start';
 $GLOBALS['TL_LANG']['CTE']['bs_tab_start'][1]     = 'Starts a Tab element.';
 $GLOBALS['TL_LANG']['CTE']['bs_tab_separator'][0] = 'Tab separator';
--- src/Resources/contao/languages/en/tl_content.php
+++ src/Resources/contao/languages/en/tl_content.php
@@ -1,15 +1,6 @@
 <?php
 
-/**
- * Contao Bootstrap
- *
- * @package    contao-bootstrap
- * @subpackage Tab
- * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2013-2020 netzmacht David Molineus. All rights reserved.
- * @license    LGPL-3.0-or-later https://github.com/contao-bootstrap/tab/blob/master/LICENSE
- * @filesource
- */
+declare(strict_types=1);
 
 $GLOBALS['TL_LANG']['tl_content']['bs_tab_name'][0]           = 'Tab name';
 $GLOBALS['TL_LANG']['tl_content']['bs_tab_name'][1]           = 'Tab name is used to identify start element in the backend.';
